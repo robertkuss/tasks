c785aa50d79c0c0b613f294036942690e9c2c025
robertkuss <robertjk@udel.edu>
2024-09-03 22:22:39 -0400

    pull request attempt 2

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App(): React.JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript Robert Kuss Hello
-                World!
+                World! .
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
