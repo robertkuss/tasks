8d2c8b3ad03aa8724aae719779cd1a41425a934d
robertkuss <robertjk@udel.edu>
2024-09-04 23:30:59 -0400

    Task 3 completed, all tests passing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): React.JSX.Element {
@@ -8,10 +9,49 @@ function App(): React.JSX.Element {
                 UD CISC275 with React Hooks and TypeScript Robert Kuss Hello
                 World! .
             </header>
+            <h1>Woah woah woah header time!!!</h1>
+            <img
+                src="images/oldter.png"
+                alt="a picture of my friend hunter if he was old"
+            />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <ul>
+                <li>I love</li>
+                <li>Listing</li>
+                <li>Things</li>
+            </ul>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: 240,
+                                height: 120,
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: 120,
+                                height: 240,
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
