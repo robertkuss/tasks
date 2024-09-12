6cdd924e5a2f9914e388f95cf7bb8e42923b407b
robertkuss <robertjk@udel.edu>
2024-09-11 20:51:41 -0400

    Finished tripleNumbers. Coded bookEndList but currently failing tests

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,6 +5,14 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
+    let bookEnded: number[] = [];
+    if (numbers.length === 1) {
+        bookEnded.push(numbers[0]);
+        bookEnded.push(numbers[0]);
+    } else if (numbers.length > 1) {
+        bookEnded.push(numbers[0]);
+        bookEnded.push(numbers[-1]);
+    }
     return numbers;
 }
 
@@ -13,7 +21,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled = numbers.map((item: number): number => item * 3);
+    return tripled;
 }
 
 /**
